66e7b32154a3c2330e6d0382177353f4071e115f
ccorgz <ccorgz70@gmail.com>
2024-01-17 06:57:01 -0300

    Feat: Version 58

--- dist/esm/components/inputs/select/select.js
+++ dist/esm/components/inputs/select/select.js
@@ -58,6 +58,16 @@ function Select(_a) {
     var _e = (0, react_1.useState)(true), isClosestToTop = _e[0], setIsClosestToTop = _e[1];
     var titleBoxRef = (0, react_1.useRef)(null);
     (0, react_1.useEffect)(function () {
+        var valueOptionLabel = optionsList === null || optionsList === void 0 ? void 0 : optionsList.filter(function (op) { return value && op[optionLabel] == value[optionLabel]; });
+        if (value && value[optionLabel]) {
+            setOptionLabelState(value[optionLabel]);
+        }
+        else if (valueOptionLabel.length > 0 &&
+            valueOptionLabel[0][optionLabel]) {
+            setOptionLabelState(valueOptionLabel[0][optionLabel]);
+        }
+    }, [value]);
+    (0, react_1.useEffect)(function () {
         if (options == optionsList) {
             return;
         }
@@ -111,10 +121,10 @@ function Select(_a) {
         react_1.default.createElement("div", { className: "reactivus-select-title-box", ref: titleBoxRef, onClick: function () {
                 setShowOptions(!showOptions);
             } },
-            optionLabelState,
+            react_1.default.createElement("span", { className: "reactivus-select-title-label" }, optionLabelState),
             react_1.default.createElement("span", { className: "reactivus-select-title-icon-close" },
-                react_1.default.createElement("svg", { width: "18", height: "18" },
-                    react_1.default.createElement("path", { d: "M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" })))),
+                react_1.default.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24", width: "18", height: "18", fill: "none", stroke: "currentColor", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" },
+                    react_1.default.createElement("path", { d: "M6 9l6 6 6-6" })))),
         react_1.default.createElement("div", { className: "reactivus-select-options-box reactivus-select-options-box-".concat(showOptions ? "show" : "hide", " reactivus-select-options-box-").concat(isClosestToTop ? "bottom" : "top"), ref: ref !== null && ref !== void 0 ? ref : null },
             filter && (react_1.default.createElement("span", { className: "reactivus-select-item-box reactivus-select-filter-box" },
                 react_1.default.createElement("input", { type: "text", placeholder: filterPlaceHolder !== null && filterPlaceHolder !== void 0 ? filterPlaceHolder : "Search", onChange: function (e) {
