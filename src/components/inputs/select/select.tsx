5a9bdc353729b2c93c907ee9a08f77bb1d644e89
ccorgz <ccorgz70@gmail.com>
2024-05-05 19:24:32 -0300

    Feat: New Types Descriptions In The Select Component

--- src/components/inputs/select/select.tsx
+++ src/components/inputs/select/select.tsx
@@ -1,32 +1,71 @@
-// IMPORTA HOOKS DO REACT
+// IMPORT REACT HOOKS
 import React, { useEffect, useRef, useState } from "react";
 
-// IMPORTA FOLHA DE ESTILOS DO COMPONENTE
+// IMPORT STYLESHEET FILE FOR THE SELECT COMPONENT
 import "../../../styles/inputs/select.css";
 
+// DEFINE THE SELECT PROPERTIES TYPES
 type SelectInputProps = {
-  icon?: any;
+  /**
+   * Text to be displayed as a label above the component.
+   */
   label?: string;
-  floatLabel?: string;
+  /**
+   * Custom value for width style of the component.
+   */
   width?: string;
+  /**
+   * React Ref reference to be set to the component.
+   */
   ref?: React.Ref<any>;
+  /**
+   * React state with value to be set to the component.
+   */
   value: any;
+  /**
+   * Array with the options to be listed in the component.
+   */
   options: Array<any>;
+  /**
+   * Name of the property to be displayed by default in the options list of the component.
+   */
   optionLabel: string;
+  /**
+   * Custom element to be rendered in the options list of the component.
+   */
   optionTemplate?: HTMLElement | any;
+  /**
+   * Boolean to control if must be rendered a filter option in the component.
+   */
   filter?: boolean;
+  /**
+   * String to be displayed as a placeholder in the filter text of the component.
+   */
   filterPlaceHolder?: string;
+  /**
+   * String containing the list of fields that the search logic mus consider in the component.
+   */
   filterBy?: string;
+  /**
+   * String to be displayed by default in the component.
+   */
   placeholder?: string;
-  onKeyDown?: any;
+  /**
+   * Boolean that controls if multi options can be selected in the component.
+   */
   multiSelect?: boolean;
+  /**
+   * Boolean that controls if the option to select all the options at once will be abled in the component.
+   */
   selectAll?: boolean;
+  /**
+   * Function that returns the value of the component.
+   */
   onChange: (selectedOption: any) => void;
 };
 
-// EXPORTA COMPONENTE POR PADRÃO
+// EXPORTS COMPONENT BY DEFAULT
 export default function Select({
-  icon,
   label,
   width,
   ref,
@@ -38,7 +77,6 @@ export default function Select({
   filterPlaceHolder,
   filterBy,
   placeholder,
-  onKeyDown,
   multiSelect,
   onChange,
   selectAll,
@@ -291,7 +329,6 @@ export default function Select({
                     }
                   });
                 } else {
-                  console.log("MULTIPLE OFF");
                   onChange && onChange({ value: option });
                   setShowOptions(false);
                 }
